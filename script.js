c43001d4cad1f0dc734a8d538f9e42f08a88a61b
MohcineDev <mouhcinedev@gmail.com>
2022-11-12 11:48:18 +0100

    verify empty value

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const myTxt = document.querySelector('#txt')
 const myHtml = document.querySelector('#html')
 const convertBtn = document.querySelector('#convert')
-
+const err = document.querySelector('.err')
 
 convertBtn.addEventListener('click', () => {
+    if (myTxt.value.trim() == '') {
+        displayError()
+        return
+    }
     let splitedTxt = myTxt.value.split(' ')
-    splitedTxt.unshift('<p>')
-    splitedTxt.push('</p>')
 
+    //splitedTxt.unshift('<p>')
+    //splitedTxt.push('</p>')
+    //avoid pace in the start and end of the string after <p>
+
+    splitedTxt[0] = '<p>' + splitedTxt[0]
+    splitedTxt[splitedTxt.length - 1] = splitedTxt[splitedTxt.length - 1] + '</p> '
+    console.log(splitedTxt)
     let newtxt = splitedTxt.join(' ')
     console.log(newtxt)
     newtxt.replace('\n', '</p><p>')
@@ -17,4 +26,11 @@ convertBtn.addEventListener('click', () => {
 })
 
 
-myHtml.addEventListener('focus', (e) => e.target.select())
+document.querySelectorAll('textarea').forEach(elem => elem.addEventListener('focus', (e) => e.target.select()))
+
+myTxt.addEventListener('focus', (e) => err.classList.remove('displayError'))
+
+
+const displayError = () => {
+    err.classList.add('displayError')
+}
